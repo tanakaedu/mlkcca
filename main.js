0d997e036faf8606380ed32e9dbcf53a1d911a7d
YuTanaka <tanakaedu@users.noreply.github.com>
2014-11-28 16:03:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var milkcocoa = new MilkCocoa("https://io-hi1wslbu4.mlkcca.com");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
-var chatDataStore = milkcocoa.dataStore("chat");
+var chatDataStore = milkcocoa.dataStore("chat2");
 var textArea, board,textName;
 window.onload = function(){
   textArea = document.getElementById("msg");
